35dad9fc7d3514546446d1f994ab82d9d8bb2012
youyunhua <youyunhua@hotmail.com>
2018-01-25 19:34:49 +0800

    * OssInfo: maxConnections, maxErrorRetry added

--- src/main/java/org/st/aliyun/ossuploader/model/OssInfo.java
+++ src/main/java/org/st/aliyun/ossuploader/model/OssInfo.java
@@ -5,61 +5,13 @@ public class OssInfo {
 	private Integer port;
 	private Integer connectionTimeOut;
 	private Integer socketTimeOut;
+	private Integer maxConnections;
+	private Integer maxErrorRetry;
 	private String bucket;
 	private String prefix;
 	private String key;
 	private String secret;
 	
-	@Override
-	public String toString() {
-		return "OssInfo [endpoint=" + endpoint + ", port=" + port + ", connectionTimeOut=" + connectionTimeOut
-				+ ", socketTimeOut=" + socketTimeOut + ", bucket=" + bucket + ", prefix=" + prefix + ", key=" + key
-				+ ", secret=" + secret + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((bucket == null) ? 0 : bucket.hashCode());
-		result = prime * result + ((endpoint == null) ? 0 : endpoint.hashCode());
-		result = prime * result + ((port == null) ? 0 : port.hashCode());
-		result = prime * result + ((prefix == null) ? 0 : prefix.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OssInfo other = (OssInfo) obj;
-		if (bucket == null) {
-			if (other.bucket != null)
-				return false;
-		} else if (!bucket.equals(other.bucket))
-			return false;
-		if (endpoint == null) {
-			if (other.endpoint != null)
-				return false;
-		} else if (!endpoint.equals(other.endpoint))
-			return false;
-		if (port == null) {
-			if (other.port != null)
-				return false;
-		} else if (!port.equals(other.port))
-			return false;
-		if (prefix == null) {
-			if (other.prefix != null)
-				return false;
-		} else if (!prefix.equals(other.prefix))
-			return false;
-		return true;
-	}
-
 	public String getEndpoint() {
 		return endpoint;
 	}
@@ -92,6 +44,22 @@ public class OssInfo {
 		this.socketTimeOut = socketTimeOut;
 	}
 	
+	public Integer getMaxConnections() {
+		return maxConnections;
+	}
+
+	public void setMaxConnections(Integer maxConnections) {
+		this.maxConnections = maxConnections;
+	}
+
+	public Integer getMaxErrorRetry() {
+		return maxErrorRetry;
+	}
+
+	public void setMaxErrorRetry(Integer maxErrorRetry) {
+		this.maxErrorRetry = maxErrorRetry;
+	}
+
 	public String getBucket() {
 		return bucket;
 	}
@@ -122,6 +90,63 @@ public class OssInfo {
 	
 	public void setSecret(String secret) {
 		this.secret = secret;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bucket == null) ? 0 : bucket.hashCode());
+		result = prime * result + ((connectionTimeOut == null) ? 0 : connectionTimeOut.hashCode());
+		result = prime * result + ((endpoint == null) ? 0 : endpoint.hashCode());
+		result = prime * result + ((port == null) ? 0 : port.hashCode());
+		result = prime * result + ((prefix == null) ? 0 : prefix.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OssInfo other = (OssInfo) obj;
+		if (bucket == null) {
+			if (other.bucket != null)
+				return false;
+		} else if (!bucket.equals(other.bucket))
+			return false;
+		if (connectionTimeOut == null) {
+			if (other.connectionTimeOut != null)
+				return false;
+		} else if (!connectionTimeOut.equals(other.connectionTimeOut))
+			return false;
+		if (endpoint == null) {
+			if (other.endpoint != null)
+				return false;
+		} else if (!endpoint.equals(other.endpoint))
+			return false;
+		if (port == null) {
+			if (other.port != null)
+				return false;
+		} else if (!port.equals(other.port))
+			return false;
+		if (prefix == null) {
+			if (other.prefix != null)
+				return false;
+		} else if (!prefix.equals(other.prefix))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "OssInfo [endpoint=" + endpoint + ", port=" + port + ", connectionTimeOut=" + connectionTimeOut
+				+ ", socketTimeOut=" + socketTimeOut + ", maxConnections=" + maxConnections + ", maxErrorRetry="
+				+ maxErrorRetry + ", bucket=" + bucket + ", prefix=" + prefix + ", key=" + key + ", secret=" + secret
+				+ "]";
 	}
 	
 }
